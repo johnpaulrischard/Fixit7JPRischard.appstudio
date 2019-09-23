e0d0f2faf1f452be6bee533fbe10d404e94d89ce
JP Rischard <john.rischard@creighton.edu>
2019-09-23 17:39:21 -0500

    Finished

--- forms/Form1/Form1.js
+++
@@ -1,1 +0,0 @@
-let quoteString = "Failure is simply the opportunity to begin again this time more intelligently."
---
+++ forms/StringMethods/StringMethods.js
@@ -0,0 +1,17 @@
+/*a*/
+let quoteString = 'Failure is simply the opportunity to begin again this time more intelligently'
+/*b*/
+let upperCaseString = quoteString.toUpperCase()
+/*c*/
+let authorString = '- Henry Ford'
+/*d*/
+let completeString = quoteString.concat(authorString)
+/*e*/
+let secondQuote = 'Talent is cheaper than table salt. What separates the talented individual from the successful one is a lot of hard work. - Stephen King'
+/*f*/
+let lowerCaseString = secondQuote.toLowerCase()
+/*g*/
+console.log(secondQuote.charAt(3))
+/*h*/
+let findString = secondQuote.slice(35, 120)
+console.log(findString)
